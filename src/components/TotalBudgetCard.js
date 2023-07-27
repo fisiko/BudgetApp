580babcd274b890e2a37ff69b6b4d3e7fef21b47
fisik <fisiko2000@gmail.com>
2023-07-27 13:07:46 +0100

    Uncategorised Expenses

---
+++ src/components/TotalBudgetCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import BudgetCard from "./BudgetCard";
+import { useBudgets } from "../contexts/BudgetsContext";
+
+export default function TotalBudgetCard() {
+  const { expenses, budgets } = useBudgets();
+  const amount = expenses.reduce((total, expense) => total + expense.amount, 0);
+  const max = budgets.reduce((total, budget) => total + budget.max, 0);
+  if (amount === 0) return null;
+  return (
+    <BudgetCard
+      amount={amount}
+      name="Uncategorized"
+      gray
+      max={max}
+      hideButtons
+    />
+  );
+}
